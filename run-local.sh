db771b36ba93040925f1b948680fc78d3e6d48b1
Thomas Read <thjread@gmail.com>
2019-02-10 23:03:01 +0000

    Working development docker compose setup

--- run-local.sh
+++ run-local.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 docker stop nginx-dev-instance
 docker rm nginx-dev-instance
-docker run --name nginx-dev-instance -d -p 80:80 nginx-dev
+docker run --name nginx-dev-instance -d -p 80:80 -p 3000:3000 nginx-dev
 chromium reminders.thjread.com
